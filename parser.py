c9bb5fb1a8da44bd903310b4053cc079f18d723e
let4ik <kozhevnikov.sv@phystech.edu>
2018-05-11 22:25:06 +0300

    parser

---
+++ parser.py
@@ -0,0 +1,71 @@
+from bs4 import BeautifulSoup
+from time import sleep
+import urllib3
+import json
+import datetime
+from urllib3.util.retry import Retry
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+months = dict(янв='1', фев='2', мар='3', апр='4', мая='5',
+              июн='6', июл='7', авг='8', сен='9', окт='10', ноя='11', дек='12')
+
+retry = Retry(connect=50, backoff_factor=2)
+http = urllib3.PoolManager(retries=retry)
+
+
+def get_items_from_ajax(url, items_number):
+    response = http.request('GET', url + '&limit={}'.format(items_number))
+    soup = BeautifulSoup(json.loads(response.data)['html'], 'lxml')
+    return soup
+
+
+def get_documents_list(theme_url, docs_number=20):
+    result = []
+    theme_id = theme_url.split('story/')[1]
+    url = 'https://www.rbc.ru/filter/ajax?story={}&offset=0'\
+        .format(theme_id)
+    soup = get_items_from_ajax(url, docs_number)
+    for item in soup.find_all('div', class_='item_story-single'):
+        upd_time = item.contents[3].find('span', class_='item__info').text.strip()
+        if len(upd_time) < 6:
+            upd_time = str(datetime.datetime.today().date().day) + ' ' +\
+                       str(datetime.datetime.today().month) + ', ' + upd_time
+        if len(upd_time) <= 13:
+            upd_time = upd_time.split(',')[0] + ' ' +\
+                       str(datetime.datetime.today().date().year) + ',' +\
+                       upd_time.split(',')[1]
+        for key, value in months.items():
+            upd_time = upd_time.replace(key, value)
+        upd_time = datetime.datetime.strptime(upd_time, '%d %m %Y, %H:%M')
+        item = item.contents[1]
+        title = item.find('span', class_='item__title').text.strip()
+        text = item.find('span', class_='item__text').text.strip()
+        link = item.get('href')
+        result.append(dict(url=link, title=title, upd_time=upd_time))
+    return result
+
+
+def get_themes(themes_number=7, docs_number=7):
+    res = []
+    url = 'https://www.rbc.ru/story/filter/ajax?offset=0'
+    soup = get_items_from_ajax(url, themes_number)
+    for item in soup.find_all('div', class_='item_story'):
+        item = item.contents[1]
+        title = item.find('span', class_='item__title').text.strip()
+        text = item.find('span', class_='item__text').text.strip()
+        link = item.get('href')
+        res.append(dict(url=link, title=title,
+                        text=text, documents_list=
+                        get_documents_list(link, docs_number)))
+    return res
+
+
+def get_document(url):
+    res = http.request('GET', url)
+    soup = BeautifulSoup(res.data, 'lxml')
+    tags = [x.text for x in soup.find_all('a', class_='article__tags__link')]
+    try:
+        text = '\n'.join([x.text for x in soup.find('div', class_='article__text').find_all('p')])
+    except AttributeError:
+        text = ''
+    return dict(text=text, tags=tags)
---
+++ update.py
@@ -0,0 +1,119 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy import Table
+from sqlalchemy import inspect
+from sqlalchemy import desc
+from sqlalchemy import and_
+import parser
+
+engine = create_engine('postgresql://localhost:5432/news_parser', echo=False)
+Base = declarative_base()
+
+
+document_tag = Table('document_tag', Base.metadata,
+                     Column('document_id', ForeignKey('document.id'),
+                            primary_key=True),
+                     Column('tag_id', ForeignKey('tag.id'),
+                            primary_key=True))
+
+theme_document = Table('theme_document', Base.metadata,
+                     Column('theme_id', ForeignKey('theme.id'),
+                            primary_key=True),
+                     Column('document_id', ForeignKey('document.id'),
+                            primary_key=True))
+
+
+class Theme(Base):
+    __tablename__ = 'theme'
+    id = Column(Integer, primary_key=True)
+    url = Column(String)
+    title = Column(String)
+    text = Column(String)
+    documents = relationship('Document', secondary=theme_document,
+                             back_populates='themes')
+
+    def __repr__(self):
+        return "<Theme(url='%s', title='%s', text='%s')>" %\
+               (self.url, self.title, self.text)
+
+
+class Document(Base):
+    __tablename__ = 'document'
+    id = Column(Integer, primary_key=True)
+    url = Column(String)
+    title = Column(String)
+    text = Column(String)
+    upd_time = Column(DateTime)
+    themes = relationship('Theme', secondary=theme_document,
+                          back_populates='documents')
+    tags = relationship('Tag', secondary=document_tag,
+                        back_populates='documents')
+
+    def __repr__(self):
+        return "<Tag(id='%s', title='%s'')>" %\
+               (self.id, self.title)
+
+
+class Tag(Base):
+    __tablename__ = 'tag'
+    id = Column(Integer, primary_key=True)
+    text = Column(String)
+    documents = relationship('Document', secondary=document_tag,
+                             back_populates='tags')
+
+    def __repr__(self):
+        return "<Tag(id='%s', text='%s'')>" %\
+               (self.id, self.text)
+
+
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+def update_database(themes_number, docs_number):
+    i = 1
+    for theme_data in parser.get_themes(themes_number, docs_number):
+        print('Theme number {} out of {}'.format(i, themes_number))Ну
+        print(theme_data['title'])
+        i += 1
+        theme = session.query(Theme).filter(Theme.url == theme_data['url']).first()
+        if not theme:
+            theme = Theme(url=theme_data['url'], title=theme_data['title'], text=theme_data['text'])
+        for document_data in theme_data['documents_list']:
+            document = session.query(Document).filter(Document.url == document_data['url']).first()
+            if not document:
+                document_data.update(parser.get_document(document_data['url']))
+                document = Document(
+                    url=document_data['url'], title=document_data['title'],
+                    text=document_data['text'],
+                    upd_time=document_data['upd_time'])
+                for tag_text in document_data['tags']:
+                    tag = session.query(Tag).filter(Tag.text == tag_text).first()
+                    if not tag:
+                        tag = Tag(text=tag_text)
+                    session.add(tag)
+                    document.tags.append(tag)
+            elif document.upd_time != document_data['upd_time']:
+                document_data.update(parser.get_document(document_data['url']))
+                document.text = document_data['text']
+                document.title = document_data['title']
+            if document not in theme.documents:
+                theme.documents.append(document)
+        session.add(theme)
+    session.commit()
+
+
+def new_docs(amount):
+    res = session.query(Document).order_by(desc(Document.upd_time))[:amount]
+    return res
+
+
+import time
+cur_time = time.time()
+update_database(40, 40)
+print('Elapsed time : {} seconds'.format(time.time() - cur_time))
